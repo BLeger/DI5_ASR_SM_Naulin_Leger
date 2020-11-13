3f8029cda05b4774d5c6949733644d80afb34c7e
Benoit LEGER <benoit.leger@hotmail.fr>
2020-11-13 20:30:09 +0100

    Retour vers la MainActivity

--- tp2b/src/main/java/polytech/example/tp2b/Activity2.java
+++ tp2b/src/main/java/polytech/example/tp2b/Activity2.java
@@ -2,8 +2,12 @@ package polytech.example.tp2b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -12,7 +16,7 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        setTitle("fenetre 2");
+        setTitle("Fenetre 2");
         //récupération du text dans le champ de saisie
         final EditText textchampsaisie = (EditText) findViewById(R.id.nameText);
         Bundle objetbunble = this.getIntent().getExtras();
@@ -23,5 +27,32 @@ public class Activity2 extends AppCompatActivity {
             // on affiche l'information dans l'edittext
             textchampsaisie.setText(InfoPasse);
         }
+
+        // Listener bouton 1
+        Button returnButton1 = findViewById(R.id.returnButton1);
+        returnButton1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToMainActivity(1);
+            }
+        });
+
+        // Listerner bouton 2
+        Button returnButton2 = findViewById(R.id.returnButton2);
+        returnButton2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToMainActivity(2);
+            }
+        });
+    }
+
+    private void returnToMainActivity(int returnCode) {
+        Intent intent = this.getIntent();
+        Bundle bundle = intent.getExtras();
+        bundle.putString("hello", "Hello World");
+
+        setResult(returnCode, intent);
+        finish();
     }
 }
