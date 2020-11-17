a1bec9eb8536031f6b7a53d892a2a80360f0eb4a
Benoit LEGER <benoit.leger@hotmail.fr>
2020-11-17 12:20:02 +0100

    Connexion au service

--- tp3/src/main/java/polytech/example/tp3/MainActivity.java
+++ tp3/src/main/java/polytech/example/tp3/MainActivity.java
@@ -2,20 +2,25 @@ package polytech.example.tp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IBackgroundServiceListener {
 
     Button btnStart;
     Button btnConnexion;
     Button btnDeconnexion;
     Button btnStop;
     EditText text;
+
+    private ServiceConnection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +45,24 @@ public class MainActivity extends AppCompatActivity {
         btnConnexion.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text.setText("CONNEXION");
+                Intent intent = new Intent(MainActivity.this, BackgroundService.class);
+
+                //Création de l’objet Connexion
+                connection = new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName name, IBinder service) {
+                        Log.i("BackgroundService", "Connected!");
+                        IBackgroundService monservice = ((BackgroundServiceBinder)service).getService();
+                        monservice.addListener(MainActivity.this);
+                    }
+
+                    public void onServiceDisconnected(ComponentName name) {
+                        Log.i("BackgroundService", "Disconnected!");
+                    }
+                };
+
+                //Connexion au servce
+                bindService(intent,connection,BIND_AUTO_CREATE);
             }
         });
 
@@ -56,6 +78,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, BackgroundService.class);
                 stopService(intent);
+            }
+        });
+    }
+
+    @Override
+    public void dataChanged(final Object data) {
+        MainActivity.this.runOnUiThread(new Runnable() {
+            public void run() {
+                text.setText(data.toString());
             }
         });
     }
